ae348ab5e7e031e574d2a60180f55ec4585f9078
Andreas Eger <dev@eger-andreas.de>
2013-02-12 15:04:52 +0100

    priny false positives/negatives

--- lib/svm_trainer/evaluator/geometric_mean.rb
+++ lib/svm_trainer/evaluator/geometric_mean.rb
@@ -14,7 +14,13 @@ module SvmTrainer
       end
       def result
         return 0.0 if @total.zero?
-        p @store if @verbose
+        if @verbose
+          p @store
+          if @store.count == 2
+            p "false positives: #{1 - @store[0][:correct].quo(@store[0][:total])}"
+            p "false negatives: #{1 - @store[1][:correct].quo(@store[1][:total])}"
+          end
+        end
         @result ||= @store.select{|e| e[:total] > 0 }
                                     .reduce(1){|a,e| a*(e[:correct].quo(e[:total]))} ** (1.quo(model.classes))
       end
